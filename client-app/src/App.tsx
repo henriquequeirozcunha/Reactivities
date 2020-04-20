c984b647d991813bf97382cbb4fe49e420dd3bb8
Henrique Queiroz <henrique.cunha@corpus.inf.br>
2020-04-19 22:54:06 -0300

    Final da Parte 3 do Curso

---
+++ client-app/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './App.css';
+import { Header, Icon, List } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>
+          Reactivities
+        </Header.Content>
+      </Header>
+
+      <List>
+        <List.Item>Apples</List.Item>
+        <List.Item>Pears</List.Item>
+        <List.Item>Oranges</List.Item>
+      </List>
+    </div>
+  );
+}
+
+export default App;
