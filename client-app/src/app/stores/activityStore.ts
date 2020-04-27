c2e957fc4c1eb9e4c0ce531f8b8593ebc2c6e446
Henrique Queiroz <henrique.cunha@corpus.inf.br>
2020-04-27 00:35:09 -0300

    Começo Seção MOBX

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,9 @@
+import {observable} from 'mobx';
+import { createContext } from 'react';
+
+class ActivityStore {
+    @observable title= 'Hello from Mobx';
+
+}
+
+export default createContext(new ActivityStore())
